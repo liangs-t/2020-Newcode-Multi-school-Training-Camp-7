c86d9019968f8039e16a28284bbce118aee1f2f7
liangs-t <1040910880@qq.com>
2020-08-01 11:51:53 +0800

    1

---
+++ 1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> P;
+const int maxn = 20550;
+const int inf = 0x3f3f3f3f;
+const int mod = 1e9 + 7;
+const ll linf = 3e18;
+#define debug(x) cout << #x << " " << x << "\n";
+
+ll power(ll x, ll y)
+{
+    ll ans = 1;
+    while (y)
+    {
+        if (y & 1)
+            ans = ans * x % mod;
+        x = x * x % mod;
+        y >>= 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}
